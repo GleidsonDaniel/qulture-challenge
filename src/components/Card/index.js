66a180e47915055d4a8880a4e3448764f9146ba5
Gleidson Daniel <gleidson10daniel@hotmail.com>
2019-10-06 10:11:35 -0300

    Add base Card component

---
+++ src/components/Card/index.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import {View, StyleSheet, Platform, TouchableOpacity} from 'react-native';
+import PropTypes from 'prop-types';
+
+const Card = ({
+  children,
+  elevation,
+  opacity,
+  cornerRadius,
+  backgroundColor,
+  onPress,
+}) => {
+  const cardStyle = Platform.select({
+    ios: () =>
+      StyleSheet.create({
+        card: {
+          shadowRadius: elevation,
+          shadowOpacity: opacity,
+          shadowOffset: {width: 0, height: elevation},
+          borderRadius: cornerRadius,
+          backgroundColor,
+          width: '95%',
+          minHeight: 60,
+          flex: 1,
+          marginTop: 15,
+          alignItems: 'center',
+          justifyContent: 'center',
+        },
+      }),
+    android: () =>
+      StyleSheet.create({
+        card: {
+          elevation,
+          borderRadius: cornerRadius,
+          backgroundColor,
+          width: '95%',
+          minHeight: 60,
+          flex: 1,
+          marginTop: 15,
+          alignItems: 'center',
+          justifyContent: 'center',
+        },
+      }),
+  })();
+
+  return (
+    <TouchableOpacity style={{alignItems: 'center'}} onPress={onPress}>
+      <View style={cardStyle.card}>{children}</View>
+    </TouchableOpacity>
+  );
+};
+
+Card.propTypes = {
+  backgroundColor: PropTypes.string,
+  elevation: PropTypes.number,
+  cornerRadius: PropTypes.number,
+  opacity: PropTypes.number,
+  onPress: PropTypes.func,
+  children: PropTypes.element,
+};
+
+Card.defaultProps = {
+  backgroundColor: '#ffffff',
+  elevation: 3,
+  cornerRadius: 5,
+  opacity: 0.5,
+  onPress: () => {},
+  children: <View />,
+};
+
+export default Card;
