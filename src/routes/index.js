67f661bed61533e6d8e32312fbb2558acdc3aad5
Gleidson Daniel <gleidson10daniel@hotmail.com>
2019-10-06 17:07:59 -0300

    Created SelectedUser screen and changed routes to support the new screen

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,41 @@
-import {createAppContainer, createSwitchNavigator} from 'react-navigation';
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
 
 import Main from '~/screens/Main';
+import SelectedUser from '~/screens/SelectedUser';
 
-const Routes = createAppContainer(createSwitchNavigator({Main}));
+import {qultureGreen} from '~/assets/colors';
+
+const Routes = createAppContainer(
+  createStackNavigator(
+    {
+      Main: {
+        screen: Main,
+        navigationOptions: {
+          title: 'Lista de Colaboradores',
+        },
+      },
+      SelectedUser: {
+        screen: SelectedUser,
+        navigationOptions: {
+          title: 'Colaborador',
+        },
+      },
+    },
+    {
+      initialRouteName: 'Main',
+      defaultNavigationOptions: {
+        headerStyle: {
+          backgroundColor: qultureGreen,
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+          textAlign: 'center',
+        },
+      },
+    },
+  ),
+);
 
 export default Routes;
