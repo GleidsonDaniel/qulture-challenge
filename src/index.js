8c5c1737f566fecd23632bd7aa67927785fca7eb
GleidsonDaniel <gleidson10daniel@hotmail.com>
2019-10-07 18:05:27 -0300

    (Feat): Adding react-native-bootsplash and android icons 1/2 - ios icons missing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
+import RNBootSplash from 'react-native-bootsplash';
 
 import {store, persistor} from '~/store';
 import Routes from '~/routes';
@@ -9,12 +10,18 @@ if (__DEV__) {
   import('~/config/ReactotronConfig');
 }
 
-const App = () => (
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <Routes />
-    </PersistGate>
-  </Provider>
-);
+const App = () => {
+  useEffect(() => {
+    RNBootSplash.hide({duration: 250});
+  }, []);
+
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Routes />
+      </PersistGate>
+    </Provider>
+  );
+};
 
 export default App;
